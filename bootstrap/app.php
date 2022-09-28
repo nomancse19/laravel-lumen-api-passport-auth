a34e30d235d340114a6ab679cb6be0e14f5dd907
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-09-28 20:32:02 +0600

    Lumen Generator Added For Laravel All general Command Uses

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -90,7 +90,7 @@ $app->configure('app');
 | totally optional, so you are not required to uncomment this line.
 |
 */
-
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
