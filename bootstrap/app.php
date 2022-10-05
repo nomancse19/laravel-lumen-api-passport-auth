186091d7ebdcdb5a1481f23cc1d14bdafb8a9ef0
nomancse19 <55144234+nomancse19@users.noreply.github.com>
2022-10-05 06:14:03 +0600

    up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,7 +95,7 @@ $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 $app->register(Laravel\Passport\PassportServiceProvider::class);
 $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
